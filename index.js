a5d3aa07911d1b58df18cfac99586cbbeaac10d4
Riooo <8907594+riooo@user.noreply.gitee.com>
2022-02-10 11:39:34 +0800

    前端合并

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', function getId(req) {
     return req.body.data
 })
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':body :method :url :status :res[content-length] - :response-time ms')
