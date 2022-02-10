9c17d64426b944b38113ca35430fa5099665de2b
Riooo <8907594+riooo@user.noreply.gitee.com>
2022-02-10 13:51:45 +0800

    小修改

--- index.js
+++ index.js
@@ -95,10 +95,31 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
 app.post('/api/notes', (request, response) => {
-    const note = request.body
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
+        id: generateId(),
+    }
+
     notes = notes.concat(note)
-    console.log(note)
+
     response.json(note)
 })
 
@@ -107,6 +128,7 @@ app.put('/api/notes/:id', (request, response) => {
     const newNote = request.body
     notes = notes.filter(note => note.id !== id)
     notes = notes.concat(newNote)
+    console.log(notes)
 })
 
 // 这是干啥的
